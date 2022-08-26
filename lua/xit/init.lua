240dd7e0b4f75f4fd9e24d2e36940ac228fa8ab4
elcih17 <hdykokd+github@gmail.com>
2022-08-26 21:02:45 +0900

    fix error in setup function when opts is nil

--- lua/xit/init.lua
+++ lua/xit/init.lua
@@ -296,7 +296,7 @@ local configured = false
 local M = {}
 
 M.setup = function(opts)
-  options = vim.tbl_deep_extend('force', options, opts)
+  options = vim.tbl_deep_extend('force', options, opts or {})
   configured = true
 
   local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
