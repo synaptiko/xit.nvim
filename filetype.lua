a0d2cf5ece9163d0aa5146cebad491de3a5c2555
Felipe Sere <github@felipesere.com>
2023-01-17 21:09:35 +0000

    Run stylua

--- filetype.lua
+++ filetype.lua
@@ -1,10 +1,10 @@
 vim.filetype.add({
-	extension = {
-		xit = "xit",
-	},
+  extension = {
+    xit = 'xit',
+  },
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "xit",
-	command = "setlocal shiftwidth=4 softtabstop=4 expandtab",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'xit',
+  command = 'setlocal shiftwidth=4 softtabstop=4 expandtab',
 })
